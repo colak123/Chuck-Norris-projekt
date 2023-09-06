95d25342da759dbaf92bffa7d0fbf7a5893d4748
colak123 <111989875+colak123@users.noreply.github.com>
2023-09-06 11:14:11 +0200

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,6 +4,7 @@
         <v-spacer></v-spacer>
         <v-btn to="/">Home</v-btn>
         <v-btn to="/chuck-norris">Chuck Norris</v-btn>
+        <v-btn to="oprojektu">O projektu</v-btn>
     </v-app-bar>
 </template>
   
