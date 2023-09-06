a9f7b2a18da1d979450b46cab276674b35356c60
colak123 <111989875+colak123@users.noreply.github.com>
2023-09-06 11:21:21 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './components/Home.vue';
-import ChuckNorris from './components/ChuckNorris.vue';
 import oprojektu from './components/OProjektu.vue';
+import ChuckNorris from './components/ChuckNorris.vue';
+
 
 const routes = [
     { path: '/', component: Home },
